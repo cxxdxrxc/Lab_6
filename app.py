8d7b081613345ccd48549491ceb3017edda4ba6d
cxxdxrxc <josecouderec@office.ufm.edu>
2025-11-06 11:20:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 import os
 import pandas as pd
 import plotly.express as px
@@ -77,5 +76,5 @@ def actualizar_grafico(depts):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000)) 
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
